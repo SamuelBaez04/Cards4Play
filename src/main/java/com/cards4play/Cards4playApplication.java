e5a1e3f2c4683f1c4156f51831e34192e9aece6d
SamuelBaez04 <samuel.valencia.7088@eam.edu.co>
2026-04-29 23:16:22 -0500

    Project Base add

---
+++ src/main/java/com/cards4play/Cards4playApplication.java
@@ -0,0 +1,13 @@
+package com.cards4play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cards4playApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cards4playApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cards4play/Cards4playApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cards4play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cards4playApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
